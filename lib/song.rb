13083e9d6fd62fc90aeda552251af231e9181e30
HANA LEANDER <hana0070@gmail.com>
2019-05-07 01:47:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
       self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
-  def self.alphabetical(song_name)
+  def self.alphabetical
     self.sort_by {|w| w.alphabetical}
   end
 end
